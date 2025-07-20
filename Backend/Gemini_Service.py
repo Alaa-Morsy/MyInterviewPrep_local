e86087d29495ef0300c0f5e302ad416f90404164
Alaa-Morsy <loli2.morsy@gmail.com>
2025-07-21 02:48:28 +0300

    Gemini si working and data is being saved properly

--- Backend/Gemini_Service.py
+++ Backend/Gemini_Service.py
@@ -9,15 +9,13 @@ async def generate_interview_questions(job_title: str,num_technical:int, num_beh
         api_key=os.getenv("GEMINI_API_KEY"),
     )
     response = client.models.generate_content(
-        model="gemini-2.5-pro",
-        prompt=f"""You are an interview preparation system asked by the user to generate {num_behavioral} behavioral Questions and {num_technical} technical Questions for his upcoming interview for a {job_title} position.""",
-        temperature=0.5,
-        max_output_tokens=1024,
+        model="gemini-2.5-flash",
+        contents=f"""You are an interview preparation system asked by the user to generate {num_behavioral} behavioral Questions and {num_technical} technical Questions for his upcoming interview for a {job_title} position.""",
         config={
-        "response_mime_type": "application/json",
-        "response_schema": list[QuestionGenerateResponse],
+            "temperature": 0.5,
+            "max_output_tokens": 1024,
+            "response_mime_type": "application/json",
+            "response_schema": list[QuestionGenerateResponse],
         }
     )
-    if response.status != 200:
-        raise Exception(f"Error generating questions: {response.status} - {response.text}")
-    return response.text
+    return response.parsed
--- Backend/main.py
+++ Backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from typing import List, Optional
+import json
 from uuid import uuid4
 import Questions as Q
 from Gemini_Service import generate_interview_questions
@@ -18,26 +19,27 @@ async def generate_questions(req: Q.GenerateRequest,SaveToUserDB: Optional[bool]
         raise HTTPException(status_code=400, detail="Job title is required")
 
     # Here integrating with Gemini Studio or another AI service
-    Q.questions = await generate_interview_questions(
-        job_title=req.job_title,
-        num_technical= req.num_technical, 
-        num_behavioral= req.num_behavioral  
-    )
-    # For now, return mock questions
-    Q.questions = [
-        Q.Question(
-            id=str(uuid4()),
+    try:
+        response = await generate_interview_questions(
             job_title=req.job_title,
-            question_type="technical",
-            question=f"What are the benefits of asynchronous programming in Python?"
-        ),
-        Q.Question(
-            id=str(uuid4()),
-            job_title=req.job_title,
-            question_type="behavioral",
-            question=f"Describe a time you solved a conflict in a team."
+            num_technical=req.num_technical,
+            num_behavioral=req.num_behavioral
         )
-    ]
+        if response is None:
+            raise HTTPException(status_code=500, detail=f"No Generated Questions : PLease try again")
+
+        Q.questions = []
+        for item in response:
+            question_obj = Q.Question(
+            id=str(uuid4()),
+            question=item.question,
+            question_type=item.question_type,
+            job_title=item.job_title,
+            )
+            Q.questions.append(question_obj)
+        
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to generate questions: {str(e)}")
 
     if SaveToUserDB:
         # Save generated questions to user DB
@@ -77,10 +79,16 @@ async def modify_and_save_questions(question_id:str, difficulty:Optional[Q.Quest
 
 @app.delete("/api/questions/{question_id}")
 async def delete_question(question_id: str):
+    id_found = False
     for q in QUESTIONS_DB_GENERATED :
         if q.id == question_id:
             QUESTIONS_DB_GENERATED.remove(q)
-        break
+            id_found = True
+            break
+    
+    if id_found != True:
+        raise HTTPException(status_code=404, detail="ID not found")
+
     return {"message": f"Question with id {question_id} deleted.",
             "Your questions": QUESTIONS_DB_GENERATED}
 
